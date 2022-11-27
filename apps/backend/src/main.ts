a978b272c4ce64b19e1d2fc17adfb437cda324c3
tiago garbi <tgbmateriais@gmail.com>
2022-11-27 18:23:49 -0300

    feat: :tada: main archive to config the project and start mongodb with apollo graphql

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,21 +1,71 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
+import { ApolloError, ApolloServer } from 'apollo-server-express';
+import { ApolloServerPluginDrainHttpServer } from 'apollo-server-core';
 import * as express from 'express';
-import * as path from 'path';
+import * as http from 'http';
+import { schema } from './app/graphql/resolvers';
+import mongoose from 'mongoose';
 
-const app = express();
+const mongodbURI = process.env.MONGODB_URI;
+const dbName = process.env.MONGODB_NAME;
 
-app.use('/assets', express.static(path.join(__dirname, 'assets')));
+const port = process.env.PORT || 8080;
 
-app.get('/api', (req, res) => {
-  res.send({ message: 'Welcome to backend!' });
-});
+export const connectDB = async (mongodbURI: string, dbName: string) => {
+  if (!mongodbURI || !dbName) {
+    return Promise.reject('MongoDB URI or DB Name is not defined');
+  }
+  try {
+    mongoose.connect(
+      mongodbURI,
+      { autoIndex: false, dbName },
+      (error) => {
+        if (error) {
+          console.log(error);
+        }
+      }
+    );
+    console.log('mongodb database started');
+    console.log(`dbURL: `, mongodbURI);
+    console.log(`dbName: `, dbName);
+    return mongoose.connection;
+  } catch (error) {
+    console.log(error);
+    return undefined;
+  }
+};
 
-const port = process.env.port || 3333;
-const server = app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}/api`);
-});
-server.on('error', console.error);
+async function startApolloServer() {
+  try {
+    await connectDB(mongodbURI, dbName);
+
+    const app = express();
+    const httpServer = http.createServer(app);
+    const server = new ApolloServer({
+      schema,
+      csrfPrevention: true,
+      cache: 'bounded',
+      plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+    });
+
+    await server.start();
+
+    server.applyMiddleware({ app });
+
+    await new Promise<void>((resolve) =>
+      httpServer.listen({ port }, resolve)
+    );
+
+    console.log(
+      `Server ready at http://localhost:${port}${server.graphqlPath}`
+    );
+  } catch (err) {
+    throw new ApolloError('Something went wrong in Apollo');
+  }
+}
+
+const server = startApolloServer();
+
+export default server;
+
+
+// mongodb+srv://cluster0.murrqem.mongodb.net/microsistec
